b3993d3e78aee0bc9383ac7f1a86b4ea4b0d4907
BBotond6 <bottondbencsics@gmail.com>
2025-12-03 23:42:09 +0100

    Create a test class

---
+++ src/test_class.py
@@ -0,0 +1,10 @@
+class test_class:
+    def is_numeric(self, arg1):
+         return arg1.isnumeric()
+    
+    def less_than_10(self, arg1):
+         if int(arg1) < 10:
+              return True
+         else:
+              return False
+
