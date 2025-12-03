e4b2d374f1dae3505bee4192cd8bdab94c0d7052
BBotond6 <bottondbencsics@gmail.com>
2025-12-03 23:47:01 +0100

    Create integration test for test class

---
+++ tests/integration/test_integration1.py
@@ -0,0 +1,18 @@
+from src.test_class import test_class
+
+def test_functionality1():
+    test_var = test_class()
+
+    test_value = "1"
+
+    if test_var.is_numeric(test_value):
+        assert test_var.less_than_10(test_value) == True
+    
+    test_value = "126"
+
+    if test_var.is_numeric(test_value):
+        assert test_var.less_than_10(test_value) == False
+    
+    test_value = "2f"
+
+    assert test_var.is_numeric(test_value) == False
